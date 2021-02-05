a5822832bf07aec1120f01e5fa8d99424513d2ab
Olya <olyabusol1605@gmail.com>
2021-02-05 21:29:18 +0200

    AngularLesson7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {RouterModule} from '@angular/router';
+import {PostModule} from "./post/post.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'posts', loadChildren: () => import('./post/post.module').then(m => m.PostModule)}
+    ]),
+    PostModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
